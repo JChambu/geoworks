b2fde05bad8daac0b6f633c332a6f9750043d03f
Sergio <asergiop21@gmail.com>
2017-03-02 11:25:26 -0300

    add seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,4 +33,15 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
- set :keep_releases, 5
+set :keep_releases, 5
+
+desc 'Runs rake db:seed'
+task :seed => [:set_rails_env] do
+  on primary fetch(:migration_role) do
+    within release_path do
+      with rails_env: fetch(:rails_env) do
+        execute :rake, "db:seed"
+      end
+    end
+  end
+end
