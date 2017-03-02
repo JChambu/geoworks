e0549c8c23468270dd588291127d0ba85362dbc7
Sergio <asergiop21@gmail.com>
2017-03-02 15:40:37 -0300

    change firstsubdomain

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -4,8 +4,8 @@
 #
 # require 'apartment/elevators/generic'
 # require 'apartment/elevators/domain'
-require 'apartment/elevators/subdomain'
-# require 'apartment/elevators/first_subdomain'
+#require 'apartment/elevators/subdomain'
+ require 'apartment/elevators/first_subdomain'
 
 #
 # Apartment Configuration
@@ -89,5 +89,5 @@ end
 # }
 
 # Rails.application.config.middleware.use 'Apartment::Elevators::Domain'
-Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
-# Rails.application.config.middleware.use 'Apartment::Elevators::FirstSubdomain'
+#Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
+ Rails.application.config.middleware.use 'Apartment::Elevators::FirstSubdomain'
