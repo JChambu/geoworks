0e7d4af9270307e28ad6f237bfbced619b4845d8
Sergio <asergiop21@gmail.com>
2017-07-12 11:41:04 -0300

    geo_editions validations

--- app/models/geo_edition.rb
+++ app/models/geo_edition.rb
@@ -4,8 +4,32 @@ class GeoEdition < ApplicationRecord
 
   attr_accessor :line
 
-  validates :poi_status_id, presence: true
+  validates :poi_status_id, presence: true, allow_blank: false
+  validates :paridad, presence: true, if: "!gw_paridad.present?"
+
+#  validates :number_door_start_original.to_i, numericality: {odd: :true}, if: "number_door_end_original.odd? && gw_paridad.nil?"
+ # validates :number_door_end_original_, numericality: {odd: :true}, if: "number_door_start_original.odd? && gw_paridad.nil?"
   
+  validates :gw_pta_ini, numericality: {only_integer: true}, allow_nil: true
+  validates :gw_pta_ini, numericality: {less_than: :gw_pta_fin }, if: "gw_pta_fin.present?"
+  validates :gw_pta_ini, numericality: {odd: :true}, if: "gw_pta_fin.odd?"
+  validates :gw_pta_ini, numericality: {even: :true}, if: "gw_pta_fin.even?"
+
+  validates :gw_pta_fin, numericality: {only_integer: true}, allow_nil: true
+  validates :gw_pta_fin, numericality: {grather_than: :gw_pta_ini}, if: "gw_pta_ini.present?"
+
+#  validate  :gw_paridad, if: :new_paridad?
+    
+  def new_paridad?
+    if :gw_paridad.present?
+      self.errors.add(:base, :gw_paridad_error)
+#    if !:gw_pta_ini.nil? && !:gw_pta_fin   
+      #return true if :gw_pta_ini.odd && :gw_pta_fin.odd && :gw_paridad.upcase == 'P'
+   # else
+      end
+    return false    
+      end
+
   def build_geom_line
 
     if self.line and !self.line.to_s.empty?
