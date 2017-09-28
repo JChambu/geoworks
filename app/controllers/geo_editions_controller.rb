a39683ea60f658c7b2015c30215958cd1ccb1691
Sergio <asergiop21@gmail.com>
2017-09-28 00:59:11 -0300

    bug controller geoeditions

--- app/controllers/geo_editions_controller.rb
+++ app/controllers/geo_editions_controller.rb
@@ -85,7 +85,7 @@ class GeoEditionsController < ApplicationController
     @segment = @geo_edition.the_geom_segment if @segment.blank? 
     @count = GeoEdition.where(user_id: current_user.id)
 
-    if !@segment.nil?
+    if !@segment.nil? and  @segment.num_points > 0
       @num_point_segment = (@segment.num_points - 1 )
       (0..@num_point_segment).each {|n|
         p @segment.point_n(n).y 
