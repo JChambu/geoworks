ea2274414abd927591641266b2738974139af3de
Sergio <asergiop21@gmail.com>
2018-05-17 09:14:34 -0300

    styles

--- app/assets/javascripts/geomaps.js
+++ app/assets/javascripts/geomaps.js
@@ -8,13 +8,36 @@ Navarra.geomaps = function (){
   
   var map = L.map('mapid').setView([51.505, -0.09], 13);
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    L.tileLayer.grayscale('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+//    L.marker([51.5, -0.09]).addTo(map)
+//        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//        .openPopup();
+
+/*    var redatos = {};
+    redatos['data_id'] =  352;
+  
+    $.ajax({
+      type: 'GET',
+      url: '/project_types/maps.json',
+      datatype: 'json',
+      data: redatos,
+      success: function(data){
+        var feature = [];
+        $.each(data, function(i,val){
+          var longitude = val['longitude'];
+          var latitude = val ['latitude'];
+          L.marker([latitude, longitude]).addTo(map)
+        })
+      }
+      })*/
+
+    var geojsonLayer = new L.geoJson.ajax("/project_types/maps.json");
+    jsonLayer.addUrl(geojsonLayer);
+
+
   }
     
     return {
