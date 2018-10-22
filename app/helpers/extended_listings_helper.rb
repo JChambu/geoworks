af6941b2f6ffa4c130378e3eb6a33d3b0c45bf5e
Sergio <asergiop21@gmail.com>
2018-10-22 20:37:20 -0300

    change type poi

--- app/helpers/extended_listings_helper.rb
+++ app/helpers/extended_listings_helper.rb
@@ -5,6 +5,6 @@ module ExtendedListingsHelper
   end
 
   def categories_for_select
-    Category.sorted_by_name.map { |category| [category.name, category.id] }
+    PoiType.sorted_by_name.map { |category| [category.name, category.id] }
   end
 end
