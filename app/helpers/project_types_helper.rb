6076db3b626d5e0b1cf2c3d51fcf3eeea770dc17
Sergio <asergiop21@gmail.com>
2018-11-24 02:54:46 -0300

    fix option select

--- app/helpers/project_types_helper.rb
+++ app/helpers/project_types_helper.rb
@@ -1,7 +1,7 @@
 module ProjectTypesHelper
 
   def region_for_select
-    ProjectField.where(project_type_id: project_type_id).select("key").map { |region|  region.key }
+    ProjectField.where(project_type_id: params[:project_type_id]).select("key").map { |region|  region.key }
   end
   
   def fields_for_select
