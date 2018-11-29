38b2dc0eff627bc5427607208949b767e5485fd6
Sergio <asergiop21@gmail.com>
2018-11-29 14:15:41 -0300

    add domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_shine_session'
+Rails.application.config.session_store :cookie_store, key: '_geoworks_session', domain: {
+  production:   '.geoworks.com.ar',
+  staging:      '.geoworks.com.ar',
+  development:  '.lvh.me'
+}.fetch(Rails.env.to_sym, :all)
