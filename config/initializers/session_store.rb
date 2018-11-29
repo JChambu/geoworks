9bc3c70ca79b9797257aec6125e380c6247b6871
Sergio <asergiop21@gmail.com>
2018-11-29 18:13:28 -0300

    disable domain session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_geoworks_session', domain: {
-  production:   '.geoworks.com.ar',
-  staging:      '.geoworks.com.ar',
-  development:  '.lvh.me'
-}.fetch(Rails.env.to_sym, :all)
+Rails.application.config.session_store :cookie_store, key: '_geoworks_session'
+#, domain: {
+#  production:   '.geoworks.com.ar',
+#  staging:      '.geoworks.com.ar',
+ # development:  '.lvh.me'
+#}.fetch(Rails.env.to_sym, :all)
