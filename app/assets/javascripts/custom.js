121f2a3d03496c46788811e798f4768e841477bb
jvillegas88 <villegas.jonathan88@gmail.com>
2018-12-15 20:06:05 -0300

    Implement data_labelling (need fix)

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -217,6 +217,7 @@ function init_chart_doughnut(size_box = null){
                 label_x_axis = value['label_x_axis'];
                 label_y_axis = value['label_y_axis_left'];
                 stacked = value['stack'];
+                data_labelling = value['data_labelling'];
               }
 
               // Extraemos el array con los datos de la serie
@@ -582,6 +583,41 @@ function init_chart_doughnut(size_box = null){
             Chart.defaults.global.defaultFontFamily = 'Source Sans Pro';
           });
 
+          if (data_labelling == true) {
+            // Define a plugin to provide data labels
+            Chart.plugins.register({
+              afterDatasetsDraw: function(chart) {
+                var ctx = chart.ctx;
+
+                chart.data.datasets.forEach(function(dataset, i) {
+                  var meta = chart.getDatasetMeta(i);
+                  if (!meta.hidden) {
+                    meta.data.forEach(function(element, index) {
+                      // Draw the text in black, with the specified font
+                      ctx.fillStyle = '#34495E';
+
+                      var fontSize = 14;
+                      var fontStyle = 'normal';
+                      var fontFamily = 'Source Sans Pro';
+                      ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
+
+                      // Just naively convert to string for now
+                      var dataString = dataset.data[index].toString();
+
+                      // Make sure alignment settings are correct
+                      ctx.textAlign = 'center';
+                      ctx.textBaseline = 'middle';
+
+                      var padding = 5;
+                      var position = element.tooltipPosition();
+                      ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
+                    });
+                  }
+                });
+              }
+            });
+
+          }
         } //cierra for data
       } //cierra function data
     }) //cierra ajax
