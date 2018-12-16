1edfd45712e3c3c0ff2bfd731c6c79cb2d30e2cc
jvillegas88 <villegas.jonathan88@gmail.com>
2018-12-16 02:26:09 -0300

    Delete old data_labelling

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -583,41 +583,6 @@ function init_chart_doughnut(size_box = null){
             Chart.defaults.global.defaultFontFamily = 'Source Sans Pro';
           });
 
-          if (data_labelling == true) {
-            // Define a plugin to provide data labels
-            Chart.plugins.register({
-              afterDatasetsDraw: function(chart) {
-                var ctx = chart.ctx;
-
-                chart.data.datasets.forEach(function(dataset, i) {
-                  var meta = chart.getDatasetMeta(i);
-                  if (!meta.hidden) {
-                    meta.data.forEach(function(element, index) {
-                      // Draw the text in black, with the specified font
-                      ctx.fillStyle = '#34495E';
-
-                      var fontSize = 14;
-                      var fontStyle = 'normal';
-                      var fontFamily = 'Source Sans Pro';
-                      ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
-
-                      // Just naively convert to string for now
-                      var dataString = dataset.data[index].toString();
-
-                      // Make sure alignment settings are correct
-                      ctx.textAlign = 'center';
-                      ctx.textBaseline = 'middle';
-
-                      var padding = 5;
-                      var position = element.tooltipPosition();
-                      ctx.fillText(dataString, position.x, position.y - (fontSize / 2) - padding);
-                    });
-                  }
-                });
-              }
-            });
-
-          }
         } //cierra for data
       } //cierra function data
     }) //cierra ajax
