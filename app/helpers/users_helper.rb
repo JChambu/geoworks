5b99f35c7eb9f34c0efafb6827c26d64479c7431
Sergio <asergiop21@gmail.com>
2019-04-22 01:49:12 -0300

    add helper user share

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,14 @@ module UsersHelper
   def users_for_select
     User.sorted_by_name.map { |user| [user.some_identifier, user.id] }
   end
+
+  def users_for_select_share_projects
+    User.where.not(id: HasProjectType.select(:user_id).where(project_type_id: params[:project_type_id])).sorted_by_name.map  { |user|
+      [user.some_identifier, user.id] }
+  end 
+
+  def users_for_select_unshare_projects
+    User.where(id: HasProjectType.select(:user_id).where(project_type_id: params[:project_type_id])).sorted_by_name.map  { |user|
+      [user.some_identifier, user.id] }
+  end 
 end
