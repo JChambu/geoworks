51c2b6089670feae47691712cfecc14c5869f0b6
Sergio <asergiop21@gmail.com>
2019-07-14 21:53:06 -0300

    add choice_list in attributes

---
+++ app/assets/javascripts/project_fields.js
@@ -0,0 +1,14 @@
+$(document).on('click', '.typef', function(event){
+
+    console.log($(this).val());
+  var option = $(this).val();
+      $('.choice_l').prop('disable', false);
+  if (option == 2){
+      $('.choice_l').prop("disable", false);
+
+  }else{
+
+  }
+
+
+})
