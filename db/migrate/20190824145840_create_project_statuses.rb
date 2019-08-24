4355320e14c5d8eb0eb66cf8b983d252237afbd0
Sergio <asergiop21@gmail.com>
2019-08-24 15:07:56 -0300

    Change name and add attribute color in project statuses

--- db/migrate/20190824145840_create_project_statuses.rb
+++ db/migrate/20190824145840_create_project_statuses.rb
@@ -2,7 +2,8 @@ class CreateProjectStatuses < ActiveRecord::Migration[5.1]
   def change
     create_table :project_statuses do |t|
       t.string :name
-      t.references :project_status, foreign_key: true
+      t.references :project_type, foreign_key: true
+      t.string :color
 
       t.timestamps
     end
