fc9c6a40fa9b7c88658dfc71692085fa274a431e
Sergio <asergiop21@gmail.com>
2019-08-26 10:37:02 -0300

    add default date in column status_update_at

--- db/migrate/20190826124752_add_column_status_update_at_to_projects.rb
+++ db/migrate/20190826124752_add_column_status_update_at_to_projects.rb
@@ -1,5 +1,5 @@
 class AddColumnStatusUpdateAtToProjects < ActiveRecord::Migration[5.1]
   def change
-    add_column :projects, :status_update_at, :datetime
+    add_column :projects, :status_update_at, :datetime, default: Time.now
   end
 end
