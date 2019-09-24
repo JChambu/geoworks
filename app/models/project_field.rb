8fa0e64c53d6f48fc057541dc6452bdfc7af0fb9
Sergio <asergiop21@gmail.com>
2019-09-24 15:46:56 -0300

    Replace whitespace for underscore in column key in fields and subfields

--- app/models/project_field.rb
+++ app/models/project_field.rb
@@ -6,6 +6,11 @@ class ProjectField < ApplicationRecord
   has_many :analytics_dashboards
   belongs_to :project_type
   belongs_to :field_type
-  
+   
   accepts_nested_attributes_for :project_subfields, allow_destroy: true
+  before_create :key_name
+
+  def key_name
+      self.key = self.name.gsub(/\s+/, '_').downcase
+  end
 end
--- app/models/project_subfield.rb
+++ app/models/project_subfield.rb
@@ -1,6 +1,10 @@
 class ProjectSubfield < ApplicationRecord
  include RailsSortable::Model
    set_sortable :sort
-  
    belongs_to :project_field
+   before_create :key_name
+
+  def key_name
+      self.key = self.name.gsub(/\s+/, '_').downcase
+  end
 end
