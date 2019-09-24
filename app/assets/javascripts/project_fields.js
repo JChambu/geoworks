c298b26b70112320df7e331ed8cda39a452cf541
jvillegas88 <villegas.jonathan88@gmail.com>
2019-09-24 19:07:46 -0300

    Show/hide + icon when appropriate #23

--- app/assets/javascripts/project_fields.js
+++ app/assets/javascripts/project_fields.js
@@ -7,4 +7,10 @@ $(document).on('click', '.typef', function(event) {
   if (option == 2 || option == 10) {
     tr.find('.choice_list').attr('disabled', false);
   }
+
+  // Sub-formulario
+  if (option == 7) {
+    tr.find('.sub_form').css("visibility", "visible");
+  }
+  
 })
