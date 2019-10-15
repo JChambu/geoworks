aa9c1c1e1489c92a4462dcb74e71dde3bfbb3ca5
Sergio <asergiop21@gmail.com>
2019-10-15 15:16:36 -0300

    Fix redirect in edit project attributes

--- app/controllers/project_types_controller.rb
+++ app/controllers/project_types_controller.rb
@@ -274,7 +274,7 @@ class ProjectTypesController < ApplicationController
   def update
     respond_to do |format|
       if @project_type.update(project_type_params)
-        format.html { redirect_to project_types_path(), notice: 'Project type was successfully updated.' }
+        format.html { redirect_to edit_project_type_path(@project_type), notice: 'Project type was successfully updated.' }
         format.json { render :show, status: :ok, location: @project_type }
       else
         format.html { render :edit }
