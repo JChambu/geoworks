a824bd9c99ed2995d1ff60ed819c70838294b136
Sergio <asergiop21@gmail.com>
2019-11-07 18:25:06 -0300

    Change name of variable ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,14 +84,14 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   #config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = { host: ENV['domain'] }
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      domain:               ENV['domain'],
-      user_name:            ENV['mail'],
-      password:             ENV['passworkd'],
+      domain:               ENV['DOMAIN'],
+      user_name:            ENV['MAIL'],
+      password:             ENV['PASSWORD'],
       authentication:       'plain',
       enable_starttls_auto: true }
   config.action_mailer.perform_caching = false
