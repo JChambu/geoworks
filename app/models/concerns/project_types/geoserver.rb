e522c944aa6c45a64ac00727cc32c5d3b4fb434d
Sergio <asergiop21@gmail.com>
2020-01-28 11:59:57 -0300

    -mSe paso a modulo independiente metodos de geoserver y colocacion de variables de entorno para accesso api geoserver

---
+++ app/models/concerns/project_types/geoserver.rb
@@ -0,0 +1,81 @@
+module ProjectTypes::Geoserver
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def reload_rgeoserver
+      require 'net/http'
+      require 'uri'
+
+      uri = URI.parse("http://localhost:8080/geoserver/rest/reload")
+      request = Net::HTTP::Post.new(uri)
+      request.basic_auth(ENV['geoserver_user'], ENV['geoserver_password'])
+      request.content_type = "text/xml"
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+      [response.body, response.code]
+    end
+
+    def exist_layer_rgeoserver( name, current_tenant = Apartment::Tenant.current  )
+
+      return false if name.nil?
+      layer = ProjectType.get_layer_rgeoserver( name, current_tenant )
+      request = [false, "400"]
+      if layer[0] 
+        request = ProjectType.reload_rgeoserver
+      else
+        name_layer = ProjectType.where(name_layer: name)
+        if name_layer
+          request = ProjectType.add_layer_geoserver(name, current_tenant)
+        end
+      end
+      request
+    end
+
+
+    def get_layer_rgeoserver( name, current_tenant = Apartment::Tenant.current )
+
+      require 'net/http'
+      require 'uri'
+
+      current_tenant = 'geoworks' if current_tenant.empty? || current_tenant == 'public' 
+      uri = URI.parse("http://localhost:8080/geoserver/rest/workspaces/geoworks/datastores/#{current_tenant}/featuretypes.json")
+      request = Net::HTTP::Get.new(uri)
+      request.basic_auth(ENV['geoserver_user'], ENV['geoserver_password'])
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+      @layers = JSON.parse(response.body)
+      @layers['featureTypes']['featureType'].each do |layer_name|
+        if name == layer_name['name']
+          return [true, response.code] 
+        end
+      end
+      return [false, response.code]
+    end
+
+    def add_layer_geoserver( name_layer, current_tenant = Apartment::Tenant.current )
+      require 'net/http'
+      require 'uri'
+      current_tenant = 'geoworks' if current_tenant.empty? || current_tenant == 'public' 
+      uri = URI.parse("http://localhost:8080/geoserver/rest/workspaces/geoworks/datastores/#{current_tenant}/featuretypes")
+      request = Net::HTTP::Post.new(uri)
+      request.basic_auth(ENV['geoserver_user'], ENV['geoserver_password'])
+      request.content_type = "text/xml"
+      request.body = "<featureType><name>#{name_layer}</name><latLonBoundingBox><minx>-180</minx><maxx>180</maxx><miny>-90</miny><maxy>90</maxy><crs>EPSG:4326</crs></latLonBoundingBox></featureType>"
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+      return [response.body, response.code]
+    end
+  end
+end
