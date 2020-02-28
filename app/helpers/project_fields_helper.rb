0780255c9c6db9873e0e1579e39496bd09e13e44
Sergio <asergiop21@gmail.com>
2020-02-28 14:46:28 -0300

    Se agregó una funcion para los campos selects que estan en indicadores #287

--- app/helpers/project_fields_helper.rb
+++ app/helpers/project_fields_helper.rb
@@ -1,2 +1,5 @@
 module ProjectFieldsHelper
+  def filter_field_for_indicator_select
+    @a = ProjectField.where(project_type_id: params[:project_type_id]).where(filter_field: true).select("name, id").ordered.map { |field|  [field.name, field.id] }
+  end
 end
