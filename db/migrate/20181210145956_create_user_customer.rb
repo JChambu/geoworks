dc4428f32526b3e1113df0bc076062d6a8aaa50e
Sergio <asergiop21@gmail.com>
2020-03-11 11:40:59 -0300

    Se agrego la columna role_id en la tabla user_customer

--- db/migrate/20181210145956_create_user_customer.rb
+++ db/migrate/20181210145956_create_user_customer.rb
@@ -3,6 +3,7 @@ class CreateUserCustomer < ActiveRecord::Migration[5.1]
     create_table :user_customers do |t|
       t.references  :user
       t.references  :customer
+      t.integer :role_id
 
       t.timestamps
     end
