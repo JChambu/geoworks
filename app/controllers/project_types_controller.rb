0e52cba447a0e1487469bcd829cf33acb493f983
Sergio <asergiop21@gmail.com>
2020-03-16 15:55:28 -0300

    Agrega el user_id a cada proyecto creado

--- app/controllers/project_types_controller.rb
+++ app/controllers/project_types_controller.rb
@@ -1,5 +1,5 @@
 class ProjectTypesController < ApplicationController
-  before_action :set_project_type, only: [:show, :edit, :update, :destroy, :geocoding]
+  before_action :set_project_type, only: [:show, :edit, :update, :destroy]
 
   # GET /project_types
   # GET /project_types.json
@@ -10,11 +10,9 @@ class ProjectTypesController < ApplicationController
   end
 
   def search_name_layer
-
     @project_name = ProjectType.where(name_layer: params[:name_projects]).pluck(:name)
     render json: {"data": @project_name}
   end
-
 
   def project_type_layers
     @projects = ProjectType.where.not(name_layer: params[:name_projects]).where(enabled_as_layer: true)
@@ -279,22 +277,19 @@ class ProjectTypesController < ApplicationController
   def destroy
     authorize! :project_types, :destroy
     @project_type.destroy
-    respond_to do |format|
-      format.html { redirect_to project_types_url, notice: 'Project type was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to root_path()
   end
 
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_project_type
     @project_type = ProjectType.find(params[:id])
-    @project_fields = @project_type.project_fields.order(:sort)
+    @project_fields = @project_type.project_fields.order(:sort) 
 
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def project_type_params
-    params.require(:project_type).permit(:name, :type_file,  :latitude, :longitude, :name_layer, :address, :department, :province, :country, :enabled_as_layer, :layer_color, :type_geometry, {file:[]}, :tracking, project_fields_attributes: [:id, :field_type_id, :name, :required, :key, :cleasing_data, :georeferenced, :regexp_type_id, {role:[]}, :sort, :key, :_destroy, :choice_list_id, :hidden, :read_only, :popup, :calculated_field, :data_script, :filter_field, :heatmap_field, :colored_points_field,  project_subfields_attributes: [:id, :field_type_id, :name, :required, :cleasing_data, :georeferenced, :regexp_type_id, :sort, :_destroy, :key, :choice_list_id, :hidden, :read_only, :popup, :calculated_field, {role:[]}, :data_script, :key]])
+    params.require(:project_type).permit(:name, :type_file,  :latitude, :longitude, :name_layer, :address, :department, :province, :country, :enabled_as_layer, :layer_color, :type_geometry, {file:[]}, :tracking, project_fields_attributes: [:id, :field_type_id, :name, :required, :key, :cleasing_data, :georeferenced, :regexp_type_id, {role:[]}, :sort, :key, :_destroy, :choice_list_id, :hidden, :read_only, :popup, :calculated_field, :data_script, :filter_field, :heatmap_field, :colored_points_field,  project_subfields_attributes: [:id, :field_type_id, :name, :required, :cleasing_data, :georeferenced, :regexp_type_id, :sort, :_destroy, :key, :choice_list_id, :hidden, :read_only, :popup, :calculated_field, {role:[]}, :data_script, :key]]).merge(user_id: current_user.id)
   end
 end
