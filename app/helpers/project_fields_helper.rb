f7f24c4414608c5e6cdd60aeaee49cd40f87c9b8
Sergio <asergiop21@gmail.com>
2020-03-27 19:58:21 -0300

    Se cambio en filtro el key por el nombre #353

--- app/helpers/project_fields_helper.rb
+++ app/helpers/project_fields_helper.rb
@@ -2,4 +2,9 @@ module ProjectFieldsHelper
   def filter_field_for_indicator_select
     @a = ProjectField.where(project_type_id: params[:project_type_id]).where(filter_field: true).select("name, id").ordered.map { |field|  [field.name, field.id] }
   end
+
+  def get_name_from_key
+    ProjectField.where(project_type_id: params[:project_type_id]).where(key: params[:q][:project_field]).select(:name).first
+  end
+
 end
