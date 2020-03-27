dd7901e083b8a1b571fe1557b7f8dd8f34d4ab71
Sergio <asergiop21@gmail.com>
2020-03-27 10:09:35 -0300

    Compartir proyectos con usuarios de la misma corporación

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,12 +8,21 @@ module UsersHelper
   end
 
   def users_for_select_share_projects
-    User.where.not(id: HasProjectType.select(:user_id).where(project_type_id: params[:project_type_id])).sorted_by_name.map  { |user|
+    current_tenant = Apartment::Tenant.current
+    customer_id = Customer.find_by(name: current_tenant)
+    User.joins(:user_customers).
+        where(user_customers:{customer_id: customer_id}).
+        where.not(id: HasProjectType.select(:user_id).
+        where(project_type_id: params[:project_type_id])).sorted_by_name.map  { |user|
       [user.some_identifier, user.id] }
   end 
 
   def users_for_select_unshare_projects
-    User.where(id: HasProjectType.select(:user_id).where(project_type_id: params[:project_type_id])).sorted_by_name.map  { |user|
+    current_tenant = Apartment::Tenant.current
+    customer_id = Customer.find_by(name: current_tenant)
+    User.joins(:user_customers).
+      where(user_customers:{customer_id: customer_id}).
+      where(id: HasProjectType.select(:user_id).where(project_type_id: params[:project_type_id])).sorted_by_name.map  { |user|
       [user.some_identifier, user.id] }
   end 
 end
