3b9e874203554d240d7b0e2ff73ae10e064f0bcd
jvillegas88 <villegas.jonathan88@gmail.com>
2020-05-27 19:28:26 -0300

    Elimina protect_from_forgery del controlador #441

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
 
-  protect_from_forgery with: :exception
-
   before_action :authenticate_user!
-  before_action :set_locale 
+  before_action :set_locale
   before_action :current_tenant
   helper_method :flashman
   layout :layout_by_resource
