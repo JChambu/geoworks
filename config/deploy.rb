24c8c3cfaa84cce821385fe54f14530af6212e67
jvillegas88 <villegas.jonathan88@gmail.com>
2020-06-13 11:06:26 -0300

    Actualiza configuración para deployar

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, "geoworks"
 set :repo_url, "https://github.com/JChambu/geoworks.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/opt/geoworks"
