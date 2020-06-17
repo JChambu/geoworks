0443b6e30899f8d48384a992ffa9fa573bcdc943
jvillegas88 <villegas.jonathan88@gmail.com>
2020-06-17 00:26:49 -0300

    Agrega validación para DELETE y UPDATE #445

--- app/models/analytics_dashboard.rb
+++ app/models/analytics_dashboard.rb
@@ -10,9 +10,16 @@ class AnalyticsDashboard < ApplicationRecord
   belongs_to :chart
   belongs_to :project_type
 
+  validate :bad_words
   validates :title, :analysis_type_id, :project_field_id, presence: :true, if: :is_basic_kpi?
   validates :title, :sql_sentence, presence: :true, if: :is_complex_kpi?
   validates :title, :sql_full, presence: :true, if: :is_advanced_kpi?
+
+  def bad_words
+    if (/DELETE|UPDATE/i.match(self.sql_full))
+      errors.add(:sql_full, "contiene una palabra no permitida")
+    end
+  end
 
   def is_basic_kpi?
     return true if self.kpi_type == 'basic'
