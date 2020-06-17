c06f8e5aeb8a345e9e1a7bdba08ca5d82f1cec99
jvillegas88 <villegas.jonathan88@gmail.com>
2020-06-17 00:26:25 -0300

    Agrega validaciones para campos nulos #445

--- app/models/analytics_dashboard.rb
+++ app/models/analytics_dashboard.rb
@@ -10,11 +10,22 @@ class AnalyticsDashboard < ApplicationRecord
   belongs_to :chart
   belongs_to :project_type
 
-  #validates :title, :analysis_type_id, :project_field_id, presence: :true, if: :is_simple_analytics?
-  #validates :title, :sql_sentence, presence: :true, unless: :is_simple_analytics?
+  validates :title, :analysis_type_id, :project_field_id, presence: :true, if: :is_basic_kpi?
+  validates :title, :sql_sentence, presence: :true, if: :is_complex_kpi?
+  validates :title, :sql_full, presence: :true, if: :is_advanced_kpi?
 
-  def is_simple_analytics?
-    return true if self.advanced_kpi == false
+  def is_basic_kpi?
+    return true if self.kpi_type == 'basic'
+    return false
+  end
+
+  def is_complex_kpi?
+    return true if self.kpi_type == 'complex'
+    return false
+  end
+
+  def is_advanced_kpi?
+    return true if self.kpi_type == 'advanced'
     return false
   end
 
