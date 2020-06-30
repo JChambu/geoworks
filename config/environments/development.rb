9506e65eba5601cdce1e551c7e89f8b0f8c6c32c
jvillegas88 <villegas.jonathan88@gmail.com>
2020-06-30 10:53:16 -0300

    Elimina código no utilizado #460

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,17 +33,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Preview mail in the browser instead of sending
-  #config.action_mailer.delivery_method = :letter_opener
-  #config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'geoworks.com.ar',
-    user_name:            'geoworks.lite@gmail.com',
-    password:             'bltpiofmltzvdeqs',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
