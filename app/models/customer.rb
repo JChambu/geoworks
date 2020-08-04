e085270a5d65df145d567263ad1db7b39016fdf5
jvillegas88 <villegas.jonathan88@gmail.com>
2020-08-04 19:21:42 -0300

    Agrega función para crear el workspace #465

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,7 +6,7 @@ class Customer < ApplicationRecord
   validates :name, :subdomain, presence: true
   validates :subdomain, uniqueness: true
 
-  after_create :create_tenant
+  after_create :create_tenant, :create_workspace_geoserver,
 
   MAPS = %w[here osm]
 
@@ -16,4 +16,24 @@ class Customer < ApplicationRecord
     return if subdomain == 'public'
     Apartment::Tenant.create(subdomain)
   end
+
+  def create_workspace_geoserver
+
+    require 'net/http'
+    require 'uri'
+    uri = URI.parse("http://#{ENV['GEOSERVER_HOST']}:8080/geoserver/rest/workspaces")
+    request = Net::HTTP::Post.new(uri)
+    request.basic_auth('admin', ENV['GEOSERVER_ADMIN_PASSWORD'])
+    request.content_type = "text/xml"
+    request.body = "<workspace><name>#{subdomain}</name></workspace>"
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+    return [response.body, response.code]
+
+  end
+
 end
