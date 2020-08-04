e4bad5d00d677f995844540abeca4442f357ad09
jvillegas88 <villegas.jonathan88@gmail.com>
2020-08-04 19:22:43 -0300

    Agrega función para crear el datastore #465

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,7 +6,7 @@ class Customer < ApplicationRecord
   validates :name, :subdomain, presence: true
   validates :subdomain, uniqueness: true
 
-  after_create :create_tenant, :create_workspace_geoserver,
+  after_create :create_tenant, :create_workspace_geoserver, :create_datastore_geoserver
 
   MAPS = %w[here osm]
 
@@ -36,4 +36,22 @@ class Customer < ApplicationRecord
 
   end
 
+  def create_datastore_geoserver
+
+    require 'net/http'
+    require 'uri'
+    uri = URI.parse("http://#{ENV['GEOSERVER_HOST']}:8080/geoserver/rest/workspaces/#{subdomain}/datastores")
+    request = Net::HTTP::Post.new(uri)
+    request.basic_auth('admin', ENV['GEOSERVER_ADMIN_PASSWORD'])
+    request.content_type = "text/xml"
+    request.body = "<dataStore><name>#{subdomain}</name><connectionParameters><host>#{ENV['POSTGRES_HOST']}</host><port>5432</port><database>#{ENV['POSTGRES_DATABASE']}</database><schema>#{subdomain}</schema><user>#{ENV['POSTGRES_USER']}</user><passwd>#{ENV['POSTGRES_PASSWORD']}</passwd><dbtype>postgis</dbtype></connectionParameters></dataStore>"
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+    return [response.body, response.code]
+
+  end
 end
