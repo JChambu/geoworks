c10a6069aa4eac25147e00fd1e515570b1d6dafb
jvillegas88 <villegas.jonathan88@gmail.com>
2020-08-10 18:31:14 -0300

    Cambia name por subdomain en users_for_select_share_projects #362

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,7 +9,7 @@ module UsersHelper
 
   def users_for_select_share_projects
     current_tenant = Apartment::Tenant.current
-    customer_id = Customer.find_by(name: current_tenant)
+    customer_id = Customer.find_by(subdomain: current_tenant)
     User.joins(:user_customers).
         where(user_customers:{customer_id: customer_id}).
         where.not(id: HasProjectType.select(:user_id).
@@ -19,7 +19,7 @@ module UsersHelper
 
   def users_for_select_unshare_projects
     current_tenant = Apartment::Tenant.current
-    customer_id = Customer.find_by(name: current_tenant)
+    customer_id = Customer.find_by(subdomain: current_tenant)
     User.joins(:user_customers).
       where(user_customers:{customer_id: customer_id}).
       where(id: HasProjectType.select(:user_id).where(project_type_id: params[:project_type_id])).sorted_by_name.map  { |user|
