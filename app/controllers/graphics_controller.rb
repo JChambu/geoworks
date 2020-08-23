a7e975aa094ff7997e4fdc62d594f65a4cb5acb2
jvillegas88 <villegas.jonathan88@gmail.com>
2020-08-23 19:00:18 -0300

    Levanta y almacena legend_display #521

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -87,8 +87,12 @@ class GraphicsController < ApplicationController
       @graphic = Graphic.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def graphic_params
-      params.require(:graphic).permit(:token, :dashboard_id, :title, :width, :label_x_axis, :label_y_axis_left, :label_y_axis_right,:stack, :tick_x_min, :tick_x_max, :tick_y_min, :tick_y_max, :step_x, :substep_x, :data_labelling, :scale, graphics_properties_attributes: [:id, :color, :chart_id, :analytics_dashboard_id, :graphic_id, :label_datasets, :left_y_axis, :point_type, :_destroy ])
-    end
+  def graphic_params
+    params.require(:graphic).permit(:token, :dashboard_id, :title, :width, :legend_display, :label_x_axis, :label_y_axis_left,
+      :label_y_axis_right, :stack, :tick_x_min, :tick_x_max, :tick_y_min, :tick_y_max, :step_x, :substep_x, :data_labelling, :scale,
+      graphics_properties_attributes: [
+        :id, :color, :chart_id, :analytics_dashboard_id, :graphic_id, :label_datasets, :left_y_axis, :point_type, :_destroy
+      ]
+    )
+  end
 end
