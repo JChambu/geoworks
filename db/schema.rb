6ce9f31f5f98ec7513af42054c235a5fea494ef9
jvillegas88 <villegas.jonathan88@gmail.com>
2020-08-24 20:34:12 -0300

    Agrega columna para almacenar el id del listado anidado #509

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200821160714) do
+ActiveRecord::Schema.define(version: 20200824223144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20200821160714) do
     t.bigint "choice_list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "nested_list_id"
     t.index ["choice_list_id"], name: "index_choice_list_items_on_choice_list_id"
   end
 
