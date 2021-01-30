711b1b78a72559d477b4fed0273744f940f75191
luchoaravena <lucho.sg2016@gmail.com>
2021-01-29 21:03:28 -0300

    Agrega campo de descripción a listados

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201214211055) do
+ActiveRecord::Schema.define(version: 20210128024355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20201214211055) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
+    t.string "description"
   end
 
   create_table "crono_jobs", force: :cascade do |t|
