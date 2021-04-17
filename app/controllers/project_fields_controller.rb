c5475c2bf2b43c89cea0d2372adcab3f86856287
Jonathan Villegas <villegas.jonathan88@gmail.com>
2021-04-16 23:40:21 -0300

    Feature/Popup multicapa (#859)
    * Mejora el popup con la posibilidad de mostrar la información de varias capas
    
    Co-authored-by: root <mf_arias@yahoo.com>

--- app/controllers/project_fields_controller.rb
+++ app/controllers/project_fields_controller.rb
@@ -39,12 +39,24 @@ class ProjectFieldsController < ApplicationController
   end
 
   def field_popup
-    @fields = ProjectField.where(project_type_id: params[:project_type_id]).where(popup: true).order(:sort)
+    project_name = params[:project_name]
+    @fields = ProjectField
+          .select('DISTINCT main.*')
+          .from('project_fields main INNER JOIN project_types sec ON sec.id = main.project_type_id')
+          .where('main.popup = ?', true)
+          .where('sec.name_layer = ?', project_name)
+          .order('sort')
+    @name = ProjectType.where(name_layer: params[:project_name]).pluck(:name)  
+
     fields_json = {}
     @fields.each do |field|
       fields_json[field.key] = field.name
     end
-    render json: fields_json
+    data = {}
+    data['project_name'] = @name
+    data['fields_popup'] = fields_json
+
+    render json: data
   end
 
   def set_project_field
