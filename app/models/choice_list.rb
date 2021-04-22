9757e1a81df5ef83bd24021e65d0373413afc114
Jonathan Villegas <villegas.jonathan88@gmail.com>
2021-04-22 17:50:56 -0300

    Corrige iteraciones en modelo choice_list

--- app/models/choice_list.rb
+++ app/models/choice_list.rb
@@ -5,44 +5,44 @@ class ChoiceList < ApplicationRecord
   accepts_nested_attributes_for :choice_list_items, reject_if: lambda { |a| a[:name].blank? }, allow_destroy: true
 
   def to_csv
-     CSV.generate(headers: true) do |csv|
-       # header
-       csv << [self.name]
-       # data
-       self.choice_list_items.each do |choice_list_item|
-         csv << [choice_list_item.name]
-       end
-     end
+    CSV.generate(headers: true) do |csv|
+      # header
+      csv << [self.name]
+      # data
+      self.choice_list_items.each do |choice_list_item|
+        csv << [choice_list_item.name]
+      end
+    end
   end
 
   def self.to_csv_all
 
-   # Busca los nombres de los listados para la cabecera del csv
+    # Busca los nombres de los listados para la cabecera del csv
     choice_lists = ChoiceList.select(:name).order(:name)
     @headers = []
     choice_lists.each do |a|
-     @headers << a.name
+      @headers << a.name
     end
 
     CSV.generate(headers: true) do |csv|
-     csv << @headers
+      csv << @headers
 
-     # Arma un objeto con todos los listados y sus items
-     cl_all = {}
-     all.order(:name).each do |choice_list|
-       choice_list_items = []
-       choice_list.choice_list_items.each do |choice_list_item|
-         choice_list_items << choice_list_item.name
-       end
-       cl_all[choice_list.name] = choice_list_items
-     end
+      # Arma un objeto con todos los listados y sus items
+      cl_all = {}
+      all.order(:name).each do |choice_list|
+        choice_list_items = []
+        choice_list.choice_list_items.each do |choice_list_item|
+          choice_list_items << choice_list_item.name
+        end
+        cl_all[choice_list.name] = choice_list_items
+      end
 
-     # Busca el listado con mayor cantidad de items
-     sql = 'SELECT MAX(a.num) FROM (SELECT COUNT(*) num FROM choice_list_items GROUP BY choice_list_id) a'
-     max = ActiveRecord::Base.connection.execute(sql)
+      # Busca el listado con mayor cantidad de items
+      sql = 'SELECT MAX(a.num) FROM (SELECT COUNT(*) num FROM choice_list_items GROUP BY choice_list_id) a'
+      max = ActiveRecord::Base.connection.execute(sql)
 
-     # Cicla el objeto y carga las filas del csv ordenadas
-     n = 0
+      # Cicla el objeto y carga las filas del csv ordenadas
+      n = 0
       loop do
         row = []
         cl_all.each do |list, items|
