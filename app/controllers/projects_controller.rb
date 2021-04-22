a05df1cfb9eab345b7383e1f1506a103346fad55
Jonathan Villegas <villegas.jonathan88@gmail.com>
2021-04-22 15:05:54 -0300

    Modifica los textos de estado de la acción

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,7 +14,7 @@ class ProjectsController < ApplicationController
       @project = Project.find(app_id)
       if @project.present?
         if @project.disable_form
-          render json: {status: 'El registro fue deshabilitado correctamente.'}
+          render json: {status: 'Registro deshabilitado.'}
         else
           render json: {status: 'Error. No se pudo deshabilitar el registro.'}
         end
@@ -37,7 +37,7 @@ class ProjectsController < ApplicationController
       @project = Project.find(app_id)
       if @project.present?
         if @project.update_form(properties)
-          render json: {status: 'El registro fue modificado correctamente.'}
+          render json: {status: 'Registro actualizado.'}
         else
           render json: {status: 'Error. No se pudo actualizar el registro.'}
         end
@@ -55,7 +55,7 @@ class ProjectsController < ApplicationController
         properties = sf['properties']
         @project_data_children = ProjectDataChild.find(child_id)
         if @project_data_children.update_subform(properties)
-          render json: {status: 'El registro fue modificado correctamente.'}
+          render json: {status: 'Registro actualizado.'}
         else
           render json: {status: 'Error. No se pudo actualizar el registro.'}
         end
@@ -72,7 +72,7 @@ class ProjectsController < ApplicationController
       @project = Project.find(app_id)
       if @project.present?
         if @project.change_owner(user_id)
-          render json: {status: 'El registro fue reasignado correctamente.'}
+          render json: {status: 'Registro reasignado.'}
         else
           render json: {status: 'Error. No se pudo reasignado el registro.'}
         end
@@ -92,9 +92,9 @@ class ProjectsController < ApplicationController
       @project = Project.find(app_id)
       if @project.present?
         if @project.change_status(status_id)
-          render json: {status: 'El registro fue reasignado correctamente.'}
+          render json: {status: 'Estado actualizado.'}
         else
-          render json: {status: 'Error. No se pudo reasignado el registro.'}
+          render json: {status: 'Error. No se pudo actualizar el estado.'}
         end
       else
         render json: {status: 'Error. No se encontró el registro.'}
