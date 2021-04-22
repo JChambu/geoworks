10f4c6c656f357facbaac337779c7c026d01616b
luchoaravena <lucho.sg2016@gmail.com>
2021-04-22 01:22:11 -0300

    Validaciones al importar listados

--- app/models/choice_list.rb
+++ app/models/choice_list.rb
@@ -5,24 +5,16 @@ class ChoiceList < ApplicationRecord
   accepts_nested_attributes_for :choice_list_items, reject_if: lambda { |a| a[:name].blank? }, allow_destroy: true
 
   def self.import(file)
-    p = 0
-    CSV.foreach(file.path, headers: true) do |row|
-      all_key = []
-      all_key = row.to_hash
-      all_key.each do |list, it|
-        if p < 1
-          @choice_list = ChoiceList.new
-          @choice_list.name = list
-          @choice_list_item = @choice_list.choice_list_items.new
-          @choice_list_item.name = it
-          @choice_list.save
-        elsif p >= 1
-          @choice_list_item = @choice_list.choice_list_items.new
-          @choice_list_item.name = it
-          @choice_list_item.save
+    headers = CSV.read(file.path, headers: true).headers
+    headers.each do |cl_name|
+      @choice_list = ChoiceList.create(name: cl_name)
+      CSV.foreach(file.path, headers: true) do |row|
+        data = row.to_hash
+        if !data[cl_name].nil?
+          @choice_list_item = @choice_list.choice_list_items.create(name: data[cl_name])
         end
       end
-      p += 1
     end
   end
+  
 end
