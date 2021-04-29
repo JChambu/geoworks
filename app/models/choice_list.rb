34370e5cd0b2c36c234255cbba0ce0826a22c607
luchoaravena <lucho.sg2016@gmail.com>
2021-04-29 18:58:50 -0300

    Elimina asociaciones

--- app/models/choice_list.rb
+++ app/models/choice_list.rb
@@ -1,8 +1,5 @@
 class ChoiceList < ApplicationRecord
   require 'csv'
-
-  has_many :project_fields
-	has_many :project_subfields
   has_many :choice_list_items, ->{order(:name)}
   accepts_nested_attributes_for :choice_list_items, reject_if: lambda { |a| a[:name].blank? }, allow_destroy: true
 
