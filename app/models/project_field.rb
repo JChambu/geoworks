d5dd5d7cf81384b99c843e10cbf14e4dffb09ffc
luchoaravena <lucho.sg2016@gmail.com>
2021-05-05 21:54:49 -0300

    Al eliminar campo padre, se elimina su campo hijo

--- app/models/project_field.rb
+++ app/models/project_field.rb
@@ -4,10 +4,10 @@ class ProjectField < ApplicationRecord
  include ProjectFields::Validations
    set_sortable :sort
 
-  has_many :project_subfields, -> {order(:sort)}
+  has_many :project_subfields, -> {order(:sort)}, dependent: :destroy
   has_many :analytics_dashboards
   belongs_to :project_type
   belongs_to :field_type
-   
+
   accepts_nested_attributes_for :project_subfields, allow_destroy: true
 end
