abd61f6c56698eb3fe5ecc53a0b92892995b2a7e
Jonathan Villegas <villegas.jonathan88@gmail.com>
2021-05-22 19:39:21 -0300

    Fix/Validar al eliminar listados y elimina items si se elimina su listado (#935)
    * Si el listado está siendo utilizado por una campo padre, un campo hijo u otro listado (anidado) arroja una advertencia
    
    * Si elimino un listados también elimino sus items
    
    Co-authored-by: luchoaravena <lucho.sg2016@gmail.com>

--- app/models/choice_list.rb
+++ app/models/choice_list.rb
@@ -1,7 +1,6 @@
 class ChoiceList < ApplicationRecord
   require 'csv'
-
-  has_many :choice_list_items, ->{order(:name)}
+  has_many :choice_list_items, -> { order(:name) }, dependent: :destroy
   accepts_nested_attributes_for :choice_list_items, reject_if: lambda { |a| a[:name].blank? }, allow_destroy: true
 
   def self.import(file)
@@ -16,7 +15,7 @@ class ChoiceList < ApplicationRecord
       end
     end
   end
-  
+
   def to_csv
     CSV.generate(headers: true) do |csv|
       # header
--- app/models/project_field.rb
+++ app/models/project_field.rb
@@ -1,13 +1,11 @@
 class ProjectField < ApplicationRecord
- include RailsSortable::Model
- include ProjectFields::Scopes
- include ProjectFields::Validations
-   set_sortable :sort
-
+  include RailsSortable::Model
+  include ProjectFields::Scopes
+  include ProjectFields::Validations
+  set_sortable :sort
   has_many :project_subfields, -> {order(:sort)}, dependent: :destroy
   has_many :analytics_dashboards
   belongs_to :project_type
   belongs_to :field_type
-
   accepts_nested_attributes_for :project_subfields, allow_destroy: true
 end
