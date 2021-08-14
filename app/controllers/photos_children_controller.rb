a20f06a06f07ae5745e0664129a97bcd27b052dc
Jonathan Villegas <villegas.jonathan88@gmail.com>
2021-08-13 21:28:31 -0300

    Agrega método show_photos_children

--- app/controllers/photos_children_controller.rb
+++ app/controllers/photos_children_controller.rb
@@ -1,2 +1,24 @@
 class PhotosChildrenController < ApplicationController
+
+  def show_photos_children
+
+    project_data_child_id = params[:project_data_child_id]
+
+    child_photos = PhotoChild
+      .where(project_data_child_id: project_data_child_id)
+      .where(row_active: true)
+
+    child_photos_array = []
+    child_photos.each do |c_photo|
+      c_photo_hash = {}
+      c_photo_hash['id'] = c_photo.id
+      c_photo_hash['name'] = c_photo.name
+      c_photo_hash['image'] = c_photo.image
+      child_photos_array.push(c_photo_hash)
+    end
+
+    render json: child_photos_array
+
+  end
+
 end
