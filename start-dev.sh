632c3fbd3cb354724dc086654ee2859d4d254213
Angel Cancio <7303724+4ng31@users.noreply.github.com>
2023-02-27 11:32:47 -0300

    Docker deploy, modificando start script

--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Waiting DB to start"
-while ! nc -z db 5432; do sleep 3; done
+while ! nc -z ${POSTGRES_HOST}  ${POSTGRES_PORT}; do sleep 3; done
 rm -f /src/tmp/pids/server.pid
 bundle exec puma -C /app/config/puma.rb
