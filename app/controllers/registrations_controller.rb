087b8aff63a8eefb44c8ba14d2f7c174eec300c1
luchoaravena <lucho.sg2016@gmail.com>
2023-03-15 00:03:43 -0300

    Change logic when role and project are searched, delete user_id from params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,14 +3,13 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    new_user = User.last.id
-    user_rol = Role.where(name: 'Público').pluck(:id).join(' ')
-    user_project = ProjectType.where(name: 'Demo Público').pluck(:id).join(' ')
+    user_rol = Role.where(name: 'Público').pluck(:id).first.to_s
+    user_project = ProjectType.where(name: 'Demo Público').pluck(:id).first.to_s
 
     params.require(:user).permit(:email, :name, :password, :password_confirmation, :country_code, :area_code, :phone)
-          .reverse_merge(user_customers_attributes: [user_id: new_user, customer_id: current_tenant.id, role_id: user_rol],
-                         has_project_types_attributes: [project_type_id: user_project, user_id: new_user],
-                         project_filters_attributes:[user_id: new_user, project_type_id: user_project, owner: true],
+          .reverse_merge(user_customers_attributes: [customer_id: current_tenant.id, role_id: user_rol],
+                         has_project_types_attributes: [project_type_id: user_project],
+                         project_filters_attributes:[project_type_id: user_project, owner: true],
                          active: true
                         )
   end
