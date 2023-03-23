55af4c0d6bd4c09bc6e6ee1136aacbcd0ac4058d
Jonathan Villegas <villegas.jonathan88@gmail.com>
2023-03-23 19:56:08 -0300

    Use merge instead of reverse_merge in user params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,11 +6,13 @@ class RegistrationsController < Devise::RegistrationsController
     user_rol = Role.find_by(name: 'Público')&.id
     user_project = ProjectType.find_by(name: 'Demo Público')&.id
 
-    params.require(:user).permit(:email, :name, :password, :password_confirmation, :country_code, :area_code, :phone)
-          .reverse_merge(user_customers_attributes: [role_id: user_rol],
-                         has_project_types_attributes: [project_type_id: user_project],
-                         project_filters_attributes:[project_type_id: user_project, owner: true],
-                         active: true
-                        )
+    params.require(:user)
+      .permit(:email, :name, :password, :password_confirmation, :country_code, :area_code, :phone)
+      .merge(
+        active: true,
+        user_customers_attributes: [role_id: user_rol],
+        has_project_types_attributes: [project_type_id: user_project],
+        project_filters_attributes:[project_type_id: user_project, owner: true]
+      )
   end
 end
