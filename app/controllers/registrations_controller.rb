723b6a9fbcfd1a677efce23b33606c4cf1d44f51
luchoaravena <lucho.sg2016@gmail.com>
2023-05-30 13:45:44 -0300

    create method in registrations_controller of Devise was modified to show message when user is registered

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,26 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        expire_data_after_sign_in!
+        redirect_to new_user_session_path, notice: "¡Registro exitoso! Ve a tu email para confirmar tu cuenta."
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
+
   private
 
   def sign_up_params
