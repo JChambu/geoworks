63f0fb49ebed4f37b211a05ac3f1cfb62e42b145
luchoaravena <lucho.sg2016@gmail.com>
2023-05-30 20:00:50 -0300

    Change logic, all project_types are asociated with has_project_types_attr

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -24,17 +24,20 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    rol_id          = Role.find_by(name: 'Público')&.id
-    project_type_id = ProjectType.find_by(name: 'Demo Público')&.id
-    current_tenant  = Apartment::Tenant.current
-    customer_id     = Customer.find_by(subdomain: current_tenant)&.id
+    byebug
+
+    rol_id            = Role.find_by(name: 'Público')&.id
+    project_types_ids = ProjectType.pluck(:id)
+    project_type_id   = ProjectType.find_by(name: 'Demo Público')&.id
+    current_tenant    = Apartment::Tenant.current
+    customer_id       = Customer.find_by(subdomain: current_tenant)&.id
 
     params.require(:user)
       .permit(:email, :name, :password, :password_confirmation, :country_code, :area_code, :phone)
       .merge(
         active: true,
         user_customers_attributes: [customer_id: customer_id, role_id: rol_id],
-        has_project_types_attributes: [project_type_id: project_type_id],
+        has_project_types_attributes: project_types_ids.map { |id| { project_type_id: id } },
         project_filters_attributes:[project_type_id: project_type_id, owner: true]
       )
   end
