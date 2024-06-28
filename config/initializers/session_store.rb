023e7dbbac30e155715bafa60486d4386acea46e
LuchoAravena <lucho.sg2016@gmail.com>
2024-06-28 17:48:21 -0300

    Logic is added to show the  associated corporations with that user and the possibility of redirection without login proccess (#1361)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 # Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_geoworks_session'
+Rails.application.config.session_store :cookie_store, key: '_geoworks_session', domain: :all, tld_length: 3
 #, domain: {
 #  production:   '.geoworks.com.ar',
 #  staging:      '.geoworks.com.ar',
