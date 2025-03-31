57bf3d60527a1df00ec9513a462023e07e15c2d4
LuchoAravena <lucho.sg2016@gmail.com>
2025-03-31 00:41:45 -0300

    Feature/geocoding (#1395)
    * Add geocoding with Nominatim based on OpenStreetMapProvider
    
    * WIP: Coordinates in geocoding search field and trace between two directions
    
    * Google Places API integration
    
    * Add API ROUTES, and change some styles in API GEOCODING
    
    * Refactor code: move styles from geomaps to custom css file
    
    * Remove alternative params from google maps uri
    
    * Move Google API KEY to .env

---
+++ app/controllers/directions_controller.rb
@@ -0,0 +1,35 @@
+class DirectionsController < ApplicationController
+  def calculate
+    require 'net/http'
+    require 'uri'
+    require 'json'
+
+    origin = params[:origin_place_id]
+    destination = params[:destination_place_id]
+
+    uri = URI("https://maps.googleapis.com/maps/api/directions/json")
+    uri.query = URI.encode_www_form({
+      origin: "place_id:#{origin}",
+      destination: "place_id:#{destination}",
+      key: ENV['GOOGLE_MAPS_API_KEY'],
+      language: 'es',
+      overview: "full"
+    })
+
+    response = Net::HTTP.get_response(uri)
+    data = JSON.parse(response.body)
+
+    if data["status"] == "OK"
+      render json: {
+        steps: data["routes"][0]["legs"][0]["steps"],
+        distance: data["routes"][0]["legs"][0]["distance"]["text"],
+        duration: data["routes"][0]["legs"][0]["duration"]["text"],
+        origin: data["routes"][0]["legs"][0]["start_location"],
+        destination: data["routes"][0]["legs"][0]["end_location"],
+        travel_mode: data["routes"][0]["legs"][0]["steps"][0]["travel_mode"]
+      }
+    else
+      render json: { error: 'No se pudo calcular la ruta' }, status: :unprocessable_entity
+    end
+  end
+end
