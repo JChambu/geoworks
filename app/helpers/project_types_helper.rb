68b14c89dff1d20a60c36c17ded0b215df229a76
luchoaravena <lucho.sg2016@gmail.com>
2025-04-09 22:30:03 -0300

    Refactor code and change some querys

--- app/helpers/project_types_helper.rb
+++ app/helpers/project_types_helper.rb
@@ -5,53 +5,53 @@ module ProjectTypesHelper
   end
 
   def region_for_select
-   @a = ProjectField.where(project_type_id: params[:project_type_id]).select("key").map { |region|  region.key }
+   @a = ProjectField
+    .select("key")
+    .where(project_type_id: params[:project_type_id])
+    .map { |region| region.key }
   end
 
-  
   def heatmap_field_for_select
-    @a = ProjectField.where(project_type_id: params[:project_type_id]).where(heatmap_field: true).select("key, name").ordered.map { |region|  [region.name, region.key] }
-  end
-
-  def interpolation_field_for_select
-    @b = ProjectField.where(project_type_id: params[:project_type_id]).where(field_type_id: 5).where.not(key: ["app_id","app_estado","app_usuario"]).select("key, name").ordered.map { |v|  [v.name, v.key] }
-    @c = ProjectSubfield
-      .select(:id, :name)
-      .joins(:project_field)
-      .where(project_fields: {project_type_id: params[:project_type_id]})
-      .where(field_type_id: 5)
-      .map { |sf| [sf.name, sf.id] }
-    @a = @b + @c
+    @a = ProjectField
+      .select("key, name")
+      .where(project_type_id: params[:project_type_id], heatmap_field: true)
+      .ordered
+      .map { |region| [region.name, region.key] }
   end
 
   def color_by_range_field_for_select
-    @b = ProjectField.where(project_type_id: params[:project_type_id]).where(field_type_id: 5).where.not(key: ["app_id","app_estado","app_usuario"]).select("key, name").ordered.map { |v|  [v.name, v.key] }
-    @c = ProjectSubfield
+    fields = ProjectField
+      .select(:name, :key)
+      .where(project_type_id: params[:project_type_id], field_type_id: 5, filter_field: true)
+      .where.not(key: ["app_id", "app_estado", "app_usuario"])
+      .order(:name)
+      .map { |f| [f.name, f.key] }
+  
+    subfields = ProjectSubfield
       .select(:id, :name)
       .joins(:project_field)
-      .where(project_fields: {project_type_id: params[:project_type_id]})
-      .where(field_type_id: 5)
+      .where(project_fields: { project_type_id: params[:project_type_id] })
+      .where(field_type_id: 5, filter_field: true)
+      .order(:name)
       .map { |sf| [sf.name, sf.id] }
-    @a = @b + @c
-  end
-
-  def color_by_attribute_field_for_select
-    @b = ProjectField.where(project_type_id: params[:project_type_id]).where(heatmap_field: true).where.not(key: ["app_id","app_estado","app_usuario"]).select("key, name").ordered.map { |v|  [v.name, v.key] }
-    @a = @b
+  
+    grouped_options = { 'Formularios' => fields, 'Subformularios' => subfields }
   end
 
   def colored_points_field_for_select
-    @a = ProjectField.where(project_type_id: params[:project_type_id]).where(colored_points_field: true).select("key, name").ordered.map { |f|  [f.name, f.key] }
+    @a = ProjectField
+      .select("key, name")
+      .where(project_type_id: params[:project_type_id], colored_points_field: true)
+      .ordered
+      .map { |f| [f.name, f.key] }
   end
 
   # Devuelve los campos padres e hijos que se van a utilizar en el select
   def filter_field_for_select
-
     fields = ProjectField
       .select(:name, :key)
-      .where(project_type_id: params[:project_type_id])
-      .where(filter_field: true)
-      .ordered
+      .where(project_type_id: params[:project_type_id], filter_field: true)
+      .order(:name)
       .map { |f| [f.name, f.key] }
 
     subfields = ProjectSubfield
@@ -59,26 +59,27 @@ module ProjectTypesHelper
       .joins(:project_field)
       .where(project_fields: {project_type_id: params[:project_type_id]})
       .where(filter_field: true)
+      .order(:name)
       .map { |sf| [sf.name, sf.id] }
 
     grouped_options = {'Formularios' => fields, 'Subformularios' => subfields}
   end
   
   def kpi_for_select
-    AnalyticsDashboard.where(project_type_id: params[:project_type_id], graph: true).select("id, title").ordered.map { |name_kpi|  [name_kpi.title, name_kpi.id]  }
+    AnalyticsDashboard.where(project_type_id: params[:project_type_id], graph: true).select("id, title").ordered.map { |name_kpi| [name_kpi.title, name_kpi.id]  }
   end
+
   def fields_for_select
     ProjectField.where(project_type_id: params["id"]).select("key").map { |region| region.key }
   end
 
   def project_types_for_select
-    ProjectType.all.map { |region| [region.name, region.id]   }
+    ProjectType.all.map { |region| [region.name, region.id] }
   end
 
   def project_types_for_layout
      ProjectType.joins(:has_project_types).where(has_project_types: {user_id: current_user.id}).ordered
   end
-
 
   def user_for_projects
     @users = User.joins("LEFT OUTER JOIN has_project_type on users.id = ")
@@ -87,5 +88,4 @@ module ProjectTypesHelper
   def project_name
     ProjectType.find(params[:project_type_id]) 
   end
-
 end
