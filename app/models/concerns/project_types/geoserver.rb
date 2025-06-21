8a6cae59955f95a6f450d4d65e5eb5506c3821f0
luchoaravena <lucho.sg2016@gmail.com>
2025-06-21 19:12:19 -0300

    WIP: Just to testing with debug in production

--- app/models/concerns/project_types/geoserver.rb
+++ app/models/concerns/project_types/geoserver.rb
@@ -3,6 +3,13 @@ module ProjectTypes::Geoserver
 
   module ClassMethods
     def reload_rgeoserver
+
+      puts ''
+      puts ' ********* reload_rgeoserver ********* '
+      p "ENTRA A Reload geoserver"
+      puts ' ********* ' 
+      puts ''
+
       require 'net/http'
       require 'uri'
 
@@ -16,25 +23,66 @@ module ProjectTypes::Geoserver
       response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
         http.request(request)
       end
+
+      puts ''
+      puts ' ********* reload_rgeoserver ********* '
+      p "Reload geoserver response"
+      p response.body
+      puts ' ********* ' 
+      puts ''
+
       [response.body, response.code]
     end
 
     def exist_layer_rgeoserver( name, current_tenant = Apartment::Tenant.current  )
+      
+      puts ''
+      puts ' ********* exist_layer_rgeoserver ********* '
+      p "Entra acá PRIMERO"
+      puts ' ********* ' 
+      puts ''
 
       return false if name.nil?
       layer = ProjectType.get_layer_rgeoserver( name, current_tenant )
+
+      puts ''
+      puts ' ********* exist_layer_rgeoserver SEXTO********* '
+      p "layer"
+      p layer
+      puts ' ********* ' 
+      puts ''
+
       request = [false, "400"]
       if layer[0]
+        puts ''
+        puts ' ********* exist_layer_rgeoserver SEPTIMO ********* '
+        p "Aca en if layer[0]"
+        puts ' ********* ' 
+        puts ''
         request = ProjectType.reload_rgeoserver
+
+        puts ''
+        puts ' ********* exist_layer_rgeoserver SEPTIMO ********* '
+        p "Aca en if layer[0]"
+        p request
+        puts ' ********* ' 
+        puts ''
       else
         name_layer = ProjectType.where(name_layer: name)
+
+        puts ''
+        puts ' ********* exist_layer_rgeoserver ELSE SEPTIMO********* '
+        p "Aca en name_layer = Project..... name_layer"
+        p name_layer
+        puts ' ********* ' 
+        puts ''
+
         if name_layer
           request = ProjectType.add_layer_geoserver(name, current_tenant)
         end
       end
       request
     end
-
 
     def get_layer_rgeoserver( name, current_tenant = Apartment::Tenant.current )
 
@@ -52,11 +100,35 @@ module ProjectTypes::Geoserver
         http.request(request)
       end
       @layers = JSON.parse(response.body)
+
+      puts ''
+      puts ' ********* layers SEGUNDO ********* '
+      p @layers
+      puts ' ********* ' 
+      puts ''
+
       @layers['featureTypes']['featureType'].each do |layer_name|
+        puts ''
+        puts ' ********* layers TERCERO EACH ********* '
+        p "SOLO PARA REVISAR QUE INGRESA"
+        puts ' ********* ' 
+        puts ''
         if name == layer_name['name']
+          puts ''
+          puts ' ********* if name == layer_name[name] CUARTO ********* '
+          p "ACA"
+          p "RESPONSE"
+          p response
+          puts ' ********* ' 
+          puts ''
           return [true, response.code]
         end
       end
+      puts ''
+      puts ' ********* FINALIZA EACH QUINTO ********* '
+      p "ACA"
+      puts ' ********* ' 
+      puts ''
       return [false, response.code]
     end
 
@@ -92,6 +164,5 @@ module ProjectTypes::Geoserver
         http.request(request)
       end
     end
-
   end
 end
